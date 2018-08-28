da3316a88b1c0348e19db82e7d26b11804821eac
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-08-28 18:14:45 +0200

    [YE-0] Fix size.

--- src/main/java/be/yildizgames/sdk/ui/SdkWindow.java
+++ src/main/java/be/yildizgames/sdk/ui/SdkWindow.java
@@ -1,7 +1,10 @@
 package be.yildizgames.sdk.ui;
 
 import be.yildizgames.module.color.Color;
+import be.yildizgames.module.window.swt.MenuBarElement;
+import be.yildizgames.module.window.swt.MenuElement;
 import be.yildizgames.module.window.swt.SwtWindow;
+import be.yildizgames.module.window.swt.TreeElement;
 import be.yildizgames.sdk.configuration.Configuration;
 import be.yildizgames.sdk.feature.project.createnew.ui.ProjectCreationWindow;
 import be.yildizgames.sdk.feature.project.createnew.util.PathUtil;
@@ -9,82 +12,41 @@ import be.yildizgames.sdk.feature.project.load.ui.ProjectLoadWindow;
 import be.yildizgames.sdk.feature.project.model.ProjectManager;
 import be.yildizgames.sdk.feature.project.save.formatter.ObjectToJson;
 import be.yildizgames.sdk.feature.project.save.persistence.ToFile;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Tree;
-import org.eclipse.swt.widgets.TreeItem;
 
 public class SdkWindow {
 
     public void init(Configuration configuration) {
         ProjectManager projectManager = new ProjectManager();
-        SwtWindow window = new SwtWindow(new Shell());
+        SwtWindow window = new SwtWindow();
         window.setWindowTitle("Yildiz-Engine SDK");
         window.setBackground(Color.rgb(50,50,50));
         window.show();
-
-        Menu menu = window.createMenuBar();
-        MenuItem fileMenuHeader = new MenuItem(menu, SWT.CASCADE);
-        fileMenuHeader.setText("&File");
-        Menu fileMenu = window.createSubMenu(fileMenuHeader);
-        MenuItem newProject = new MenuItem(fileMenu, SWT.PUSH);
-        newProject.setText("&New project");
-        newProject.addSelectionListener(
-                new SelectionAdapter() {
-                    @Override
-                    public void widgetSelected(SelectionEvent selectionEvent) {
-                        new ProjectCreationWindow(window, projectManager).init(configuration);
-                    }
-                }
-        );
-        MenuItem loadProject = new MenuItem(fileMenu, SWT.PUSH);
-        loadProject.setText("&Load project");
-        loadProject.addSelectionListener(
-                new SelectionAdapter() {
-                    @Override
-                    public void widgetSelected(SelectionEvent selectionEvent) {
-                        new ProjectLoadWindow(window).init(configuration);
-                    }
-                }
-        );
-        MenuItem saveProject = new MenuItem(fileMenu, SWT.PUSH);
-        saveProject.setText("&Save");
-        saveProject.addSelectionListener(
-                new SelectionAdapter() {
-                    @Override
-                    public void widgetSelected(SelectionEvent selectionEvent) {
-                        projectManager.getProject().ifPresent(p -> ToFile.save(PathUtil.getRoot(p, configuration), ObjectToJson.fromProject(p)));
-                    }
-                }
-        );
-        MenuItem saveasProject = new MenuItem(fileMenu, SWT.PUSH);
-        saveasProject.setText("&Save as");
+        this.generateMenus(window, projectManager, configuration);
         this.generateProjectTree(window);
         window.run();
     }
 
-    private void generateProjectTree(SwtWindow parent) {
-        final Tree tree = parent.createTree();
-        tree.setSize(200, parent.getHeight());
-        TreeItem scene = new TreeItem(tree, 0);
-        scene.setText("Scene");
-        TreeItem model = new TreeItem(scene, 0);
-        model.setText("Models");
-        TreeItem light = new TreeItem(scene, 0);
-        light.setText("Lights");
-        TreeItem particle = new TreeItem(scene, 0);
-        particle.setText("Particles");
-        TreeItem camera = new TreeItem(scene, 0);
-        camera.setText("Cameras");
-        TreeItem material = new TreeItem(tree, 0);
-        material.setText("Materials");
-        TreeItem audio = new TreeItem(tree, 0);
-        audio.setText("Audios");
-        TreeItem ui = new TreeItem(tree, 0);
-        ui.setText("UI");
+    private void generateMenus(SwtWindow parent, ProjectManager projectManager, Configuration configuration) {
+        parent.createMenuBar(
+                new MenuBarElement("File",
+                        new MenuElement("New", e ->  new ProjectCreationWindow(parent, projectManager).init(configuration)),
+                        new MenuElement( "Open", e -> new ProjectLoadWindow(parent).init(configuration)),
+                        new MenuElement("Save", e -> projectManager.getProject().ifPresent(p -> ToFile.save(PathUtil.getRoot(p, configuration), ObjectToJson.fromProject(p))))));
     }
+
+    private void generateProjectTree(SwtWindow parent) {
+        parent.createTree(150, parent.getHeight(),
+                new TreeElement("Project",
+                        new TreeElement("Scene",
+                                new TreeElement("Models"),
+                                new TreeElement("Lights"),
+                                new TreeElement("Particles"),
+                                new TreeElement("Cameras")),
+                        new TreeElement("Materials"),
+                        new TreeElement("Audios",
+                                new TreeElement("SFX"),
+                                new TreeElement("Musics")),
+                        new TreeElement("UI")));
+    }
+
 }
