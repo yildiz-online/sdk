aa64c39336e746c29441813be6c12ffbe0f880f8
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-09-12 22:59:47 +0200

    [YE-0] Add box.

---
+++ src/main/java/be/yildizgames/sdk/feature/project/model/items/BoxDefinition.java
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ *  Copyright (c) 2018 Grégory Van den Borre
+ *
+ *  More infos available: https://www.yildiz-games.be
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
+ */
+package be.yildizgames.sdk.feature.project.model.items;
+
+import be.yildizgames.common.geometry.Point3D;
+import be.yildizgames.common.model.EntityId;
+
+public class BoxDefinition {
+
+    private String name;
+
+    private EntityId id;
+
+    private Point3D position;
+
+    private Point3D direction;
+
+    private String material;
+
+    private Point3D size;
+
+    private boolean staticObject;
+
+    public Point3D getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point3D position) {
+        this.position = position;
+    }
+
+    public Point3D getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Point3D direction) {
+        this.direction = direction;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public Point3D getSize() {
+        return size;
+    }
+
+    public void setSize(Point3D size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EntityId getId() {
+        return id;
+    }
+
+    public void setId(EntityId id) {
+        this.id = id;
+    }
+
+    public boolean isStaticObject() {
+        return this.staticObject;
+    }
+
+    public void setStaticObject(boolean staticObject) {
+        this.staticObject = staticObject;
+    }
+}
--- src/main/java/be/yildizgames/sdk/feature/project/render/Renderer.java
+++ src/main/java/be/yildizgames/sdk/feature/project/render/Renderer.java
@@ -1,9 +1,34 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ *  Copyright (c) 2018 Grégory Van den Borre
+ *
+ *  More infos available: https://www.yildiz-games.be
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
+ */
 package be.yildizgames.sdk.feature.project.render;
 
 import be.yildizgames.common.libloader.NativeResourceLoader;
+import be.yildizgames.common.shape.Box;
 import be.yildizgames.module.coordinate.Coordinates;
 import be.yildizgames.module.coordinate.Position;
 import be.yildizgames.module.coordinate.Size;
+import be.yildizgames.module.graphic.GraphicObject;
 import be.yildizgames.module.graphic.GraphicWorld;
 import be.yildizgames.module.graphic.material.Material;
 import be.yildizgames.module.graphic.ogre.OgreGraphicEngine;
@@ -15,6 +40,7 @@ import be.yildizgames.module.window.swt.SwtWindow;
 import be.yildizgames.module.window.swt.SwtWindowEngine;
 import be.yildizgames.sdk.feature.project.ProjectListener;
 import be.yildizgames.sdk.feature.project.model.Project;
+import be.yildizgames.sdk.feature.project.model.items.BoxDefinition;
 import be.yildizgames.sdk.feature.project.model.items.ParticleEmitterDef;
 import be.yildizgames.sdk.feature.project.model.items.ParticleSystemDefinition;
 import be.yildizgames.sdk.feature.project.model.items.Scene;
@@ -70,6 +96,21 @@ public class Renderer implements ProjectListener {
     public void createParticleSystem(Scene scene, ParticleSystemDefinition def) {
         //TODO Add definition to graphic API
         this.createParticleSystem(this.worlds.get(scene.getName()), def);
+    }
+
+    public void createBox(Scene scene, BoxDefinition def) {
+        //TODO Add definition to graphic API
+        this.createBox(this.worlds.get(scene.getName()), def);
+    }
+
+    private void createBox(GraphicWorld w, BoxDefinition def) {
+        Box s = Box.box((int)def.getSize().x, (int)def.getSize().y, (int)def.getSize().z);
+        GraphicObject o;
+        if(def.isStaticObject()) {
+            o = w.createStaticObject(def.getId(), s, Material.get(def.getMaterial()), def.getPosition(), def.getDirection());
+        } else {
+            o = w.createMovableObject(def.getId(), s, Material.get(def.getMaterial()), def.getPosition());
+        }
     }
 
     private void createParticleSystem(GraphicWorld w, ParticleSystemDefinition def) {
